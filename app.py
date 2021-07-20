7a126c39367babaa642e511f3bd6b2f3bd9763c4
Megan Howley <meglhowley@gmail.com>
2021-07-20 16:28:26 -0400

    deleted test.py

--- app.py
+++ app.py
@@ -8,18 +8,24 @@ from models.db import db
 from alembic import op
 from datetime import datetime
 import os
-from flask import render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 # Init db and migrate here
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/trending_db"
-app.config['SQLALCHEMY_ECHO'] = True
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace(
+        "://", "ql://", 1)
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.env = 'production
+else:
+    app.debug = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/trending_db'
+    app.config['SQLALCHEMY_ECHO'] = True
 
 # Init db and migrate here
 db.init_app(app)
@@ -39,22 +45,6 @@ api.add_resource(post.PostDetail, '/posts/<int:post_id>')
 api.add_resource(like.Likes, '/likes')
 api.add_resource(like.LikeDetail, '/likes/<int:like_id>')
 
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload():
-#     if request.method == 'POST':
-#         # save the single "profile" file
-#         file = request.files['image']
-#         newFile= Image(name=file.filename, data=file.read())
-#         db.session.add(newFile)
-#         db.session.commit()
-#         return ('Saved to the database!')
-#     images= Image.find_all()
-#     for x in images:
-#         print(x.name)
-#         print(x.data)
-#     return('hi')
-#     # images.get_data()
-#     # return images
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
