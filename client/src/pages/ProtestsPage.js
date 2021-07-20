287f5619e05ddb510c8d01779d38d5fa62c4c83e
Megan Howley <meglhowley@gmail.com>
2021-07-20 08:41:29 -0400

    added san fran and protests pages

--- client/src/pages/ProtestsPage.js
+++ client/src/pages/ProtestsPage.js
@@ -1,17 +1,31 @@
 import ScrollAnimation from 'react-animate-on-scroll'
 import Lottie from 'react-lottie'
 import essential from '../animations/essential.json'
-import DownArrowBlack from '../components/DownArrowBlack'
+import DownArrowWhite from '../components/DownArrowWhite'
+import blm from '../animations/blm.json'
 
 const ProtestsPage = (props) => {
   return (
-    <div ref={props.protestsPageRef} className="page-section">
+    <div ref={props.protestsPageRef} className="page-section protests-section">
       <div>
         May 30th: A state of emergency is declared in Los Angeles County and
         city of Los Angeles because of protests over the death of George Floyd
         and racial injustice. Curfews are also declared in Philadelphia and
         Atlanta.
       </div>
+      <Lottie
+        options={{
+          loop: true,
+          autoplay: true,
+          animationData: blm,
+          rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+          }
+        }}
+        isClickToPauseDisabled={true}
+        height={350}
+        width={350}
+      />
       <div
         onClick={() =>
           props.junePageRef.current.scrollIntoView({
@@ -20,7 +34,7 @@ const ProtestsPage = (props) => {
         }
         className="next protests"
       >
-        <DownArrowBlack />
+        <DownArrowWhite />
       </div>
     </div>
   )
--- client/src/pages/SanFran.js
+++ client/src/pages/SanFran.js
@@ -1,11 +1,11 @@
 import ScrollAnimation from 'react-animate-on-scroll'
 import Lottie from 'react-lottie'
 import essential from '../animations/essential.json'
-import DownArrowBlack from '../components/DownArrowBlack'
+import DownArrowWhite from '../components/DownArrowWhite'
 
 const SanFranPage = (props) => {
   return (
-    <div ref={props.sanFranPageRef} className="page-section">
+    <div ref={props.sanFranPageRef} className="page-section san-fran-section">
       <div>
         Aug. 16: The SCU Lightning Complex fires start, affecting several Bay
         Area counties. Almost 400,000 acres are burned, making it the third
@@ -19,7 +19,7 @@ const SanFranPage = (props) => {
         }
         className="next san-fran"
       >
-        <DownArrowBlack />
+        <DownArrowWhite />
       </div>
     </div>
   )
