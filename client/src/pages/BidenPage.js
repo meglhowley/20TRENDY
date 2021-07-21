8d189592bfdb748d00b6706a3ddf6564008abbb1
Megan Howley <meglhowley@gmail.com>
2021-07-21 09:04:31 -0400

    added vote page

--- client/src/pages/BidenPage.js
+++ client/src/pages/BidenPage.js
@@ -1,19 +1,21 @@
 import ScrollAnimation from 'react-animate-on-scroll'
 import Lottie from 'react-lottie'
 import essential from '../animations/essential.json'
-import DownArrowBlack from '../components/DownArrowBlack'
+import DownArrowWhite from '../components/DownArrowWhite'
 
 const BidenPage = (props) => {
   return (
     <div ref={props.bidenPageRef} className="page-section biden-page">
       <div>
         Nov 7: After days of waiting, major news organizations declare that Joe
-        Biden has secured enough electoral college votes to win the presidency.
-        <br />
-        Kamala Harris becomes first female, first black, and first
-        Asian-American elected VP.
+        Biden has secured enough electoral <br />
+        college votes to win the presidency. Kamala Harris becomes first female,
+        first black, and first Asian-American elected VP.
       </div>
-      <img src="https://media0.giphy.com/media/55m7McmQ9tcD26kQ3I/giphy.gif" />
+      <img
+        className="vote-gif"
+        src="https://media0.giphy.com/media/55m7McmQ9tcD26kQ3I/giphy.gif"
+      />
       <div
         onClick={() =>
           props.novemberPageRef.current.scrollIntoView({
@@ -22,7 +24,7 @@ const BidenPage = (props) => {
         }
         className="next biden"
       >
-        <DownArrowBlack />
+        <DownArrowWhite />
       </div>
     </div>
   )
